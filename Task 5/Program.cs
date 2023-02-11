24c2f66caef3360954e963d484b1ddaad8d5654e
Valentina <west.77@mail.ru>
2023-02-11 13:47:01 +0300

    Task 5 added

---
+++ Task 5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число:   ");
+int N = Convert.ToInt32(Console.ReadLine()?? "");
+int GetNumbersComposition(int x)
+{
+    int comp = 1;
+    for (int num = 1; num <= x; num++)
+    {
+        comp = comp * num;
+    }
+    return comp;
+}
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {GetNumbersComposition(N)}");
