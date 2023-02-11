80743a255bf615de3e26609ebe92e50ac29c3d96
Valentina <west.77@mail.ru>
2023-02-11 13:05:29 +0300

    Task 4 added

---
+++ Task 4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()?? "");
+int i = number;
+int result = 0;
+while( i != 0)
+{
+    i = i/10;
+    result++;
+}
+Console.WriteLine("Количество цифр в числе  " + number + "  =  " + result);
+
