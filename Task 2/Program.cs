aa08cbfb2b56a184cc1fc41e6cc1b438dfbd4f9b
Valentina <west.77@mail.ru>
2023-02-11 11:18:40 +0300

    Task 2 added

---
+++ Task 2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму чисел от 1 до А
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for(int i=1; i<=number; i++)
+{
+    sum = sum + i;
+    Console.WriteLine();
+    Console.WriteLine(sum);
+}
