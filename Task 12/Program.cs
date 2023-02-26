f543e1a970dcca6ea48eefd5e7ca039b43ed8282
Valentina <west.77@mail.ru>
2023-02-26 15:03:21 +0300

    Task 12 added

--- Task 12/Program.cs
+++ Task 12/Program.cs
@@ -1,41 +1,17 @@
 ﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
-int size = new Random().Next(10, 26);
-int min = 1;
-int max = 100;
-System.Console.WriteLine($"Количество элементов массива: {size}");
-
-int[] FillArray(int size, int min, int max)
+Random rnd = new Random();             
+int[] array = new int[8];              
+int sum = 0;                         
+for (int i = 0; i < array.Length; i++) 
 {
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++)
+    array[i] = rnd.Next(0, 99);        
+    if (i % 2 != 0)                    
     {
-        array[i] = new Random().Next(min, max + 1);
+        sum = sum + array[i];        
     }
-    return array;
 }
-
-// первый вариант - собираем новый массив из произведений пар.
-// В случае, если количество элементов исходного массива нечетное, среднее значение массива "выпадает" из обработки
-
-int[] GetPairProduct(int[] data)
-{
-    int[] product = new int[data.Length / 2];
-    int last = data.Length - 1;
-    for (int i = 0; i < data.Length / 2; i++)
-    {
-        product[i] = data[i] * data[last];
-        last = last - 1;
-    }
-    return product;
-}
-
-void PrintArray(int[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        System.Console.Write(arr[i] + " ");
-    }
-    System.Console.WriteLine();
-}
-
+var result = string.Join(", ", array);
+Console.WriteLine(" ");           
+Console.WriteLine($"[{result}]");
+Console.WriteLine($"Сумма чисел на нечетных: {sum}");
