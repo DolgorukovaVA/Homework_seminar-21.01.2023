add9cfa9c331690fc0a8caafb2ea90ded6851f98
Valentina <west.77@mail.ru>
2023-02-26 13:59:56 +0300

    Task 7 changed

--- Task 7/Program.cs
+++ Task 7/Program.cs
@@ -18,8 +18,8 @@ void PrintArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
+        Console.Write($" {array[i]} ");
     }
 }
-
+Console.WriteLine("  ");
 PrintArray(CreateArray(N, min, max));
